86e0b467f29bf6c0b0dec6bd4ee93db119b7ff52
elisachiriacogit config --global user.email <elisachiriaco22@outlook.it>
2022-05-16 16:00:13 +0200

    fine header

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
+    <header>
     <AppHeader />
+    </header>
   </div>
 </template>
 
@@ -18,4 +20,27 @@ export default {
 <style lang="scss">
 @import "./style/generals.scss";
 @import "./style/vars.scss";
+
+header{
+  background-image: url(./assets/images/hero-header-bakery.jpg);
+  background-size: cover;
+  width: 100%;
+  height: 600px;
+}
+.subtitle{
+  text-transform: uppercase;
+  color: $mamba;
+}
+.mybutton-white{
+  background-color: $white;
+  width: 150px;
+  height: 35px;
+  border-radius: 4px;
+  line-height: 35px;
+  a{
+    text-decoration: none;
+    color: $daisy;
+    font-size: 12px;
+  }
+  }
 </style>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -6,24 +6,40 @@
         </div>
         <div class="col-8 justify-content-end">
         <ul>
-          <li><a href="#">Home</a></li>
-          <li><a href="#">Shop</a></li>
-          <li><a href="#">About</a></li>
-          <li><a href="#">Gallery</a></li>
-          <li><a href="#">Locations</a></li>
-          <li><a href="#">Journal</a></li>
-          <li><a href="#">Contact</a></li>
-          <li><a href="#">My Account</a></li>
+          <li><a href="#">{{myArray[0].nome}}</a></li>
+          <li><a href="#">{{myArray[1].nome}}</a></li>
+          <li><a href="#">{{myArray[2].nome}}</a></li>
+          <li><a href="#">{{myArray[3].nome}}</a></li>
+          <li><a href="#">{{myArray[4].nome}}</a></li>
+          <li><a href="#">{{myArray[5].nome}}</a></li>
+          <li><a href="#">{{myArray[6].nome}}</a></li>
+          <li><a href="#">{{myArray[7].nome}}</a></li>
           <li><a href="#"><i class="fa-solid fa-cart-shopping"></i></a></li>
         </ul>
+      </div>
+      <div class="row header-bg">
+        <div class="col-4">
+          <span class="subtitle">Fresh & tasty bakery every day</span>
+          <h1>The Perfect Fresh Bread</h1>
+          <p>Cras consequat lectus vestibulum tortor pulvinar, quis euismod nisl varius. Aliquam erat volutpat. Nullam quis sagittis nibh ac nulla id orci tempor.</p>
+          <div class="mybutton-white text-center">
+            <a href="#">Explore our products</a>
+          </div>
+        </div>
       </div>
       </div>
     </div>
 </template>
 
 <script>
+import dati from '../dati.json'
 export default {
   name: 'AppHeader',
+  data(){
+    return{
+      myArray: dati,
+    }
+  }
 }
 </script>
 
@@ -41,5 +57,14 @@ li{
     color: $daisy;
     text-decoration: none;
   }
+}
+h1{
+  color: $daisy;
+}
+p{
+  color: $mamba;
+}
+.header-bg{
+  padding-top: 100px;
 }
 </style>
